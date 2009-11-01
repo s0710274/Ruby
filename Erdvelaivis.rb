c3d1d5078b3dbdb51aefafa48efdf492999d16d6
Julius <Julius6731@gmail.com>
2009-11-01 19:40:53 +0200

    smulkus papildymai

--- Erdvelaivis.rb
+++ Erdvelaivis.rb
@@ -1,10 +1,11 @@
 class Erdvelaivis
-  def initialize(modelis, pagam_data, vietos)
+  def initialize(modelis, pagam_data, vietos, greitis)
     @pavadinimas = modelis
     @pagaminimo_data = pagam_data
     @vietos = vietos
+    @greitis = greitis
   end
-  attr_reader :modelis, :pagaminimo_data, :vietos
+  attr_reader :modelis, :pagaminimo_data, :vietos, :greitis
 
 
 end
--- erdvelaivis_spec.rb
+++ erdvelaivis_spec.rb
@@ -2,7 +2,7 @@ require 'erdvelaivis'
 
 describe Erdvelaivis do
   before(:each) do
-    @erdvelaivis = Erdvelaivis.new('C34-5D', '2001-01-12', 12)
+    @erdvelaivis = Erdvelaivis.new('C34-5D', '2001-01-12', 12, 3000)
   end
 
   it "should have modelis" do
@@ -15,6 +15,10 @@ describe Erdvelaivis do
 
   it "should have vietos" do
     @erdvelaivis.should respond_to(:vietos)
+  end
+
+  it "should have greitis" do
+    @erdvelaivis.should respond_to(:greitis)
   end
   
 end
